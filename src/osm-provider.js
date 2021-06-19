4c80eef0b5719f11113d6da77e9623f26aabc507
Manvi <65354230+manvi-123894@users.noreply.github.com>
2021-06-19 21:39:45 +0530

    Delete src directory

--- src/osm-provider.js
+++
@@ -1,6 +0,0 @@
-export const osm = {
- maptiler : {
-  url : "https://api.maptiler.com/maps/basic/256/{z}/{x}/{y}@2x.png?key=SwFxbVSPbF0mJyPzKBGz",
-  attribution : '&copy;<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>'
- }
-}
